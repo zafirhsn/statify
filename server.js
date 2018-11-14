44a85497772f180a2e5faced38a555498bf5669c
Zafir Hasan <zafirhsn@gmail.com>
2018-11-14 01:31:23 -0500

    Minor changes

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
-const app = require('express')();
+const express = require('express');
+const app = express();
 
 app.listen(3000, ()=> {
   console.log('Server is listening on port 3000...');
 });
+
+app.use(express.static(__dirname + '/public/html'));
+// app.use(express.static('public/css'));
+
