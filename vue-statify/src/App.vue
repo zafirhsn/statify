f65ea92d1892b63ba67eca19d4264a5d1cf633f5
Zafir Hasan <zafirhsn@gmail.com>
2020-02-14 16:16:33 -0500

    :recycle: Adding Vue to project

---
+++ vue-statify/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+}
+</script>
+
+<style>
+
+</style>
