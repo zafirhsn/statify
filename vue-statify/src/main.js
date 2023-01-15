b2217f9775c78347235364282fd5db2cc5a2bc79
Zafir Hasan <zafirhsn@gmail.com>
2023-01-15 17:06:44 -0500

    feat: add bugsnag

--- vue-statify/src/main.js
+++ vue-statify/src/main.js
@@ -5,7 +5,17 @@ import VueRouter from 'vue-router';
 import {routes} from './routes.js'
 import {store} from './store/store'
 import vuetify from './plugins/vuetify';
+import Bugsnag from '@bugsnag/js';
+import BugsnagPluginVue from '@bugsnag/plugin-vue';
 
+Bugsnag.start({
+  apiKey: process.env.VUE_APP_BUGSNAG_API_KEY,
+  plugins: [new BugsnagPluginVue()]
+})
+
+const bugsnagVue = Bugsnag.getPlugin('vue');
+bugsnagVue.installVueErrorHandler(Vue);
+window.Bugsnag = Bugsnag;
 
 Vue.config.productionTip = false
 
